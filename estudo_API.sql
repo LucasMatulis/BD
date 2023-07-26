0ee8e5d3ad371e2afe7c1df94494ddc62326d695
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2023-07-26 11:13:03 -0300

    sdfsdf

--- estudo_API.sql
+++ estudo_API.sql
@@ -5,4 +5,12 @@ use exemplo_pi;
 create table cliente(
 id int primary key auto_increment,
 nome varchar(100),
-cpf varchar(11))
+cpf varchar(11));
+
+select * from cliente;
+
+insert into cliente(nome,cpf) values('Lucas','35542458874');
+
+update cliente set nome='joao' where id=1;
+
+delete from cliente where id =1;
