0790991f3a4119da36a5ccbecda8ae3f72c0374f
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2023-07-31 13:30:31 -0300

    sda

--- estudo_API.sql
+++ estudo_API.sql
@@ -14,3 +14,10 @@ insert into cliente(nome,cpf) values('Lucas','35542458874');
 update cliente set nome='joao' where id=1;
 
 delete from cliente where id =1;
+
+CREATE TABLE usuario (
+    id INTEGER PRIMARY KEY auto_increment NOT NULL, 
+    nome VARCHAR(100) NOT NULL,
+    usuario VARCHAR(100) NOT NULL,
+    admin BOOLEAN    
+  );
